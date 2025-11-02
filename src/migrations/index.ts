c84f660ff77831c68e23b0ae7c34eca35641e44f
Jacob Palomo <jacob.palomo248@gmail.com>
2025-11-01 18:22:10 -0600

    fix: database vercel errors (with migrations)

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251102_002134 from './20251102_002134';
+
+export const migrations = [
+  {
+    up: migration_20251102_002134.up,
+    down: migration_20251102_002134.down,
+    name: '20251102_002134'
+  },
+];
