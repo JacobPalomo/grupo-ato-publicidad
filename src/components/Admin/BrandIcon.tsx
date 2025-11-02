bbc6d99c4e3e72c246156f472a123c14da4b82da
Jacob Palomo <jacob.palomo248@gmail.com>
2025-11-01 21:49:51 -0600

    feat: new BrandIcon & new BrandLogo

--- src/components/Admin/BrandIcon.tsx
+++ src/components/Admin/BrandIcon.tsx
@@ -11,7 +11,7 @@ export const BrandIcon: React.FC = () => {
       decoding="async"
       height={64}
       loading="eager"
-      src="/media/logo-500x500.png"
+      src="https://www.grupoatopublicidad.com/logo-500x500.png"
       width={64}
     />
   )
--- src/components/Admin/BrandLogo.tsx
+++ src/components/Admin/BrandLogo.tsx
@@ -7,11 +7,11 @@ export const BrandLogo: React.FC = () => {
     /* eslint-disable @next/next/no-img-element */
     <img
       alt="Grupo ATO Publicidad"
-      className="h-10 w-auto"
+      className="h-10 w-auto invert dark:invert-0"
       decoding="async"
       height={64}
       loading="eager"
-      src="/media/logo.png"
+      src="https://www.grupoatopublicidad.com/logo.png"
       width={240}
     />
   )
