d7ad90bfc3e1e8d9adf080c29a88b5229fc8b38e
Jacob Palomo <jacob.palomo248@gmail.com>
2025-11-01 19:24:33 -0600

    fix: upload media content

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -11,6 +11,7 @@ import { fileURLToPath } from 'url'
 import { anyone } from '../access/anyone'
 import { authenticated } from '../access/authenticated'
 
+const usingVercelBlob = Boolean(process.env.BLOB_READ_WRITE_TOKEN)
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
@@ -38,43 +39,49 @@ export const Media: CollectionConfig = {
       }),
     },
   ],
-  upload: {
-    // Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
-    staticDir: path.resolve(dirname, '../../public/media'),
-    adminThumbnail: 'thumbnail',
-    focalPoint: true,
-    imageSizes: [
-      {
-        name: 'thumbnail',
-        width: 300,
-      },
-      {
-        name: 'square',
-        width: 500,
-        height: 500,
-      },
-      {
-        name: 'small',
-        width: 600,
-      },
-      {
-        name: 'medium',
-        width: 900,
-      },
-      {
-        name: 'large',
-        width: 1400,
-      },
-      {
-        name: 'xlarge',
-        width: 1920,
-      },
-      {
-        name: 'og',
-        width: 1200,
-        height: 630,
-        crop: 'center',
-      },
-    ],
-  },
+  upload: (() => {
+    const baseConfig: NonNullable<CollectionConfig['upload']> = {
+      adminThumbnail: 'thumbnail',
+      focalPoint: true,
+      imageSizes: [
+        {
+          name: 'thumbnail',
+          width: 300,
+        },
+        {
+          name: 'square',
+          width: 500,
+          height: 500,
+        },
+        {
+          name: 'small',
+          width: 600,
+        },
+        {
+          name: 'medium',
+          width: 900,
+        },
+        {
+          name: 'large',
+          width: 1400,
+        },
+        {
+          name: 'xlarge',
+          width: 1920,
+        },
+        {
+          name: 'og',
+          width: 1200,
+          height: 630,
+          crop: 'center',
+        },
+      ],
+    }
+
+    if (!usingVercelBlob) {
+      baseConfig.staticDir = path.resolve(dirname, '../../public/media')
+    }
+
+    return baseConfig
+  })(),
 }
